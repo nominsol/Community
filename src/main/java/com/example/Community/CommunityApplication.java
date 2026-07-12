09198b5f13917f593caec2e52e9211af6b25b5a2
nominsol <xmxmxm3577@naver.com>
2026-07-12 22:51:16 +0900

    테스트 주석

--- src/main/java/com/example/Community/CommunityApplication.java
+++ src/main/java/com/example/Community/CommunityApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class CommunityApplication {
-
+	//테스트 주석
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
 	}
